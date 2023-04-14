8dd20136df1319fd79e1883c9666c671203ea1a5
angenehms <angenehms@gmail.com>
2023-04-14 20:34:19 +0900

    Kiosk.js class constructor complete and add app-section shadow design

--- src/js/component/Kiosk.js
+++ src/js/component/Kiosk.js
@@ -1,21 +1,25 @@
 class Kiosk {
     constructor () {
         const appCalculate = document.querySelector(".app-section-calculate");
-        this.btnItem = appCalculate.querySelector(".btn-item");
-        this.btnInput = appCalculate.querySelector(".btn-input-money");
-        this.btnReturn = appCalculate.querySelector(".btn-balance-return");
+        this.btnItem = appCalculate.querySelector(".btn-item"); // 메뉴버튼
+        this.btnInput = appCalculate.querySelector(".btn-input-money"); // 입금버튼
+        this.btnReturn = appCalculate.querySelector(".btn-balance-return"); // 반환버튼
+        this.myMoney = appCalculate.querySelector(".amount-mymoney"); // 소지금텍스트
+        this.inputMoney = appCalculate.querySelector(".inp-put"); // 입금액인풋태그
+        this.balance = appCalculate.querySelector(".amount-balance"); // 잔액텍스트
 
         const appPayment = document.querySelector(".app-section-payment");
-        this.btnCancel = appPayment.querySelector(".btn-cancel");
-        this.btnPay = appPayment.querySelector(".btn-payment-decision");
-        this.btnReset = appPayment.querySelector(".btn-all-reset");
+        this.btnCancel = appPayment.querySelector(".btn-cancel"); // 메뉴취소버튼
+        this.btnPay = appPayment.querySelector(".btn-payment-decision"); // 결제버튼
+        this.btnReset = appPayment.querySelector(".btn-all-reset"); // 처음으로버튼
+        this.totalPayed = appPayment.querySelector(".amount-total") // 가격텍스트
 
         const appPayedModal = document.querySelector("#modal-payed");
-        this.btnIssueReceipt = appPayedModal.querySelector(".btn-issue-receipt");
-        this.btnUnissueReceipt = appPayedModal.querySelector(".btn-unissue-receipt");
+        this.btnIssueReceipt = appPayedModal.querySelector(".btn-issue-receipt"); // 발행버튼 
+        this.btnUnissueReceipt = appPayedModal.querySelector(".btn-unissue-receipt"); // 미발행버튼
 
         const appReceiptModal = document.querySelector("#modal-receipt");
-        this.btnCloseReceipt = appReceiptModal.querySelector(".btn-close-receipt");
+        this.btnCloseReceipt = appReceiptModal.querySelector(".btn-close-receipt"); // 닫기버튼
     }
 
     setup () {
@@ -23,6 +27,11 @@ class Kiosk {
     }
 
     bindEvents () {
+
+        // 입금버튼
+        this.btnInput.addEventListener("click", () => {
+            
+        })
 
         // 결제버튼 클릭시 영수증 발행여부 모달창 열기 기능
         this.btnPay.addEventListener("click", () => {
