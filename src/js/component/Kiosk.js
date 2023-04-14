c700f3c7b2fe2551c78544c5fc02e207b94afc4c
angenehms <angenehms@gmail.com>
2023-04-14 18:47:05 +0900

    modal on/off feat complete when btnPay is clicked

--- src/js/component/Kiosk.js
+++ src/js/component/Kiosk.js
@@ -15,7 +15,7 @@ class Kiosk {
         this.btnUnissueReceipt = appPayedModal.querySelector(".btn-unissue-receipt");
 
         const appReceiptModal = document.querySelector("#modal-receipt");
-        this.btnClosereceipt = appReceiptModal.querySelector(".btn-close-receipt");
+        this.btnCloseReceipt = appReceiptModal.querySelector(".btn-close-receipt");
     }
 
     setup () {
@@ -24,8 +24,24 @@ class Kiosk {
 
     bindEvents () {
 
+        // 결제버튼 클릭시 영수증 발행여부 모달창 열기 기능
+        this.btnPay.addEventListener("click", () => {
+            document.querySelector("#modal-payed").style.display = "block";
+        })
+
+        // 영수증 발행여부 모달창에서 발행 클릭시 해당 모달창 종료와 영수증 모달창 열기
+        this.btnIssueReceipt.addEventListener("click", () => {
+            document.querySelector("#modal-payed").style.display = "none";
+            document.querySelector("#modal-receipt").style.display = "block";
+        })
+
+        // 영수증 발행여부 모달창에서 미발행 클릭시 해당 모달창 종료
+        this.btnUnissueReceipt.addEventListener("click", () => {
+            document.querySelector("#modal-payed").style.display = "none";
+        })
+
         // 영수증 모달창 close 버튼
-        this.btnClosereceipt.addEventListener("click", () => {
+        this.btnCloseReceipt.addEventListener("click", () => {
             document.querySelector("#modal-receipt").style.display = "none";
         })
 
