8eac8d9b66afcf96c8457684f3e7f43244531e8f
angenehms <angenehms@gmail.com>
2023-04-19 16:06:22 +0900

    feat : modal-receipt only open when staged list exist && print  order-date, order-number, amount-payed on the receipt

--- src/js/component/Kiosk.js
+++ src/js/component/Kiosk.js
@@ -97,10 +97,11 @@ class Kiosk {
 
             const StagedItem = document.querySelectorAll(".app-section-payment .item-list-staged li");
 
-            // staged 리스트 없으면 결제할 상품을 선택해주세요! 창 띄우기 
+            // 결제버튼 클릭시 : staged 리스트 없으면 결제할 상품을 선택해주세요! 창 띄우기            
+            if ( StagedItem.length ) {
 
-            // 영수증 모달에 staged 리스트 뿌리기
-            StagedItem.forEach((item) => {
+                // 결제버튼 클릭시 : 영수증 모달에 staged 리스트 뿌리기
+                StagedItem.forEach((item) => {
 
                 const modalStagedList = document.querySelector("#modal-receipt .item-list-staged");
                 const modalStagedItem = document.createElement("li");
@@ -117,13 +118,54 @@ class Kiosk {
 
                 modalStagedItem.innerHTML = modalStagedItemTemplate;
                 modalStagedList.appendChild(modalStagedItem);
-            })
+                });
 
-            // 발행일시 현재시간 출력해야함
-            // 주문번호 난수 출력해야함
-            // 결제금액 데이터 받아와야함
 
-            document.querySelector("#modal-payed").style.display = "block";
+                // 결제버튼 클릭시 : 모달창에 발행일시 현재시간 뿌려주기
+                const orderDate = document.querySelector("#modal-receipt .order-date"); // 발행일시 div
+
+                const now = new Date();
+                const year = now.getFullYear();
+                const month = now.getMonth() + 1;
+                const day = now.getDate();
+                const hour = now.getHours();
+                const minute = now.getMinutes();
+                const second = now.getSeconds();
+
+                const ampm = hour >= 12 ? '오후' : '오전';
+                const formattedHour = hour % 12 === 0 ? 12 : `0${hour % 12}`;
+                const formattedMonth = month < 10 ? `0${month}` : month;
+                const formattedDay = day < 10 ? `0${day}` : day;
+                const formattedMinute = minute < 10 ? `0${minute}` : minute;
+                const formattedSecond = second < 10 ? `0${second}` : second;
+
+                const formattedDate = `${year}-${formattedMonth}-${formattedDay}`;
+                const formattedTime = `${ampm} ${formattedHour}:${formattedMinute}:${formattedSecond}`;
+
+                const formattedDateTime = `${formattedDate} ${formattedTime}`;
+
+                orderDate.textContent = `발 행 일 시 : ${formattedDateTime}`;
+
+
+                // 결제버튼 클릭시 : 모달창에 주문번호 난수 뿌려주기
+                const orderNumber = document.querySelector("#modal-receipt .order-number"); // 주문번호 div
+                const orderNumberVal = Math.floor(Math.random() * 99) + 1;
+                orderNumber.textContent = `주 문 번 호 : ${orderNumberVal}`;
+
+
+                // 결제버튼 클릭시 : 모달창에 결제금액 데이터 받아서 뿌려주기
+                const amountPayed = document.querySelector("#modal-receipt .amount-payed"); // 결제금액 div
+                const amountPayedVal = document.querySelector(".app-section-payment .amount-total").textContent;
+                amountPayed.textContent = `결 제 금 액 : ${amountPayedVal}`;
+
+                document.querySelector("#modal-payed").style.display = "block";
+
+            } else {
+
+                alert("결제할 상품을 선택해주세요!");
+
+            }
+              
         })
 
         // 영수증 발행여부 모달창에서 발행 클릭시 해당 모달창 종료와 영수증 모달창 열기
